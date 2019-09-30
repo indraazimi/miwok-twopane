24b7d1f067cc582eb3bec4cdc5afa2cdcf8fce69
Indra Azimi <indraazimi@yahoo.com>
2019-09-30 12:45:33 +0700

    Migrate to AndroidX

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -18,7 +18,7 @@ package com.example.android.miwok;
 import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
